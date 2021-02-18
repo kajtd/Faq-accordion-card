d21762a5b20367658613e5396982adbdf7aead98
Kajetan Domagala <kajtekd8@gmail.com>
2021-02-18 17:23:59 +0100

    Add desktop version
    Added mobile version of faq, box animation and improved font styles.

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const buttons = [...document.querySelectorAll(".faq__btn")];
 const questions = [...document.querySelectorAll(".faq__question")];
 const answers = [...document.querySelectorAll(".faq__answer")];
+const box = document.querySelector('.box');
 
 function toggleFaq() {
   let index = buttons.indexOf(this);
-  console.log(index)
   const buttonToggle = this.getAttribute('aria-expanded');
-  console.log(buttonToggle)
   for (i=0; i<buttons.length; i++) {
     buttons[i].setAttribute('aria-expanded', 'false');
     buttons[i].classList.remove("faq__btn--active");
@@ -15,11 +14,13 @@ function toggleFaq() {
   }
   
   if (buttonToggle === 'false') {
+    box.classList.add("box--active");
     questions[index].classList.add("faq__question--active");
     answers[index].classList.add("faq__answer--active");
     this.classList.add("faq__btn--active");
     this.setAttribute('aria-expanded', 'true');
   }
+  else box.classList.remove("box--active");
 }
 
 buttons.forEach(button => button.addEventListener('click', toggleFaq));
